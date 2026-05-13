a51ea7adf3417641b0e2e03f88c3ae48aef97cb9
Vu-Cee95 <ST10468302@rcconnect.edu.za>
2026-05-13 18:46:33 +0200

    Add chatbot memory and conversation state models

---
+++ CybersecurityChatbotGUI/Models/ConversationState.cs
@@ -0,0 +1,8 @@
+﻿namespace CybersecurityChatbotGUI.Models
+{
+    public class ConversationState
+    {
+        public string CurrentTopic { get; set; } = "";
+        public int FollowUpCount { get; set; } = 0;
+    }
+}
---
+++ CybersecurityChatbotGUI/Models/UserMemory.cs
@@ -0,0 +1,10 @@
+﻿namespace CybersecurityChatbotGUI.Models
+{
+    public class UserMemory
+    {
+        public string UserName { get; set; } = "Cyber Friend";
+        public string FavouriteTopic { get; set; } = "";
+        public string LastTopic { get; set; } = "";
+        public string LastSentiment { get; set; } = "";
+    }
+}
